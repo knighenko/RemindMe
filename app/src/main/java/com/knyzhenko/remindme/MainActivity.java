665a3c10b348788b9e5b57cd16b945a5fa9a72a5
User <knighenko@gmail.com>
2022-06-21 23:06:47 +0200

    Add Toolbar.xml

--- app/src/main/java/com/knyzhenko/remindme/MainActivity.java
+++ app/src/main/java/com/knyzhenko/remindme/MainActivity.java
@@ -3,12 +3,28 @@ package com.knyzhenko.remindme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.toolbar_title);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
